8de7b66c8bbe85ffbd868572a3fbc4bd2b6fc8e9
Jure Ham <jure@hamsworld.net>
2013-12-01 01:18:36 +0100

    Save stats to file prototype

--- monitor.py
+++ monitor.py
@@ -14,24 +14,45 @@ import settings
 local_address = (sys.argv[1], int(sys.argv[2]))
 servers = [(addr, port) for addr, port in settings.SERVER_LIST if addr != local_address[0] or port != local_address[1]]
 
-statsd.config(settings.STATSD_HOST, settings.STATSD_PROJECT_NAME)
-statsc = statsd.get()
+statsc = None
+if settings.STATSD_HOST:
+	statsd.config(settings.STATSD_HOST, settings.STATSD_PROJECT_NAME)
+	statsc = statsd.get()
+
 
 stats_queue = Queue()
 
 
 def flush_stats():
+
+	stats_files = {}
+	def open_file(mtype, local_server_name, server_name):
+		fname = '%s%s_%s_%s.log' % (settings.STATS_LOG_DIR, mtype, local_server_name, server_name)
+		if fname in stats_files:
+			return stats_files[fname]
+
+		stats_files[fname] = open(fname, 'a', buffering=1)
+		return stats_files[fname]
+
 	while True:
-		mtype, server, val = stats_queue.get(block=True)
+		t, mtype, server, val = stats_queue.get(block=True)
 		logging.debug('STATS: %s %s %s', mtype, server, val)
 
 		local_server_name = local_address[0]
 		server_name = server[0]
 
-		if mtype == 'timer':
-			statsc.timer('tcp_beep.%s.%s' % (local_server_name, server_name), val)
-		elif mtype == 'error':
-			statsc.counter('tcp_error.%s.%s.%s' % (local_server_name, server_name, val), 1)
+		if statsc:
+			if mtype == 'timer':
+				statsc.timer('tcp_beep.%s.%s' % (local_server_name, server_name), val)
+			elif mtype == 'error':
+				statsc.counter('tcp_error.%s.%s.%s' % (local_server_name, server_name, val), 1)
+
+		if settings.WRITE_STATS_TO_FILE:
+			f = open_file(mtype, local_server_name, server_name)
+			if mtype == 'timer':
+				f.write('%s %s\n' % (t, val))
+			elif mtype == 'error':
+				f.write('%s %s\n' % (t, val))
 
 def single_client(server):
 
@@ -49,13 +70,13 @@ def single_client(server):
 
 				td = (time.time() - t0) * 1000
 
-				stats_queue.put(('timer', server, td))
+				stats_queue.put((t0, 'timer', server, td))
 
 				time.sleep(settings.MESSAGE_INTERVAL)
 
 			sock.send("QUIT\n")
 		except Exception:
-			stats_queue.put(('error', server, 'connection_error'))
+			stats_queue.put((time.time(), 'error', server, 'connection_error'))
 		finally:
 			sock.close()
 			time.sleep(1)
